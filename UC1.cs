92486c3453dd608be7a0ded6b88139553d945a92
Shree Thakre <shreet365@gmail.com>
2022-12-18 10:58:36 +0530

    Adding class Employee present or not

---
+++ UC1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeUC
+{
+    class UC1
+    {
+        public static void EmployeePresent(int IS_FULL_TIME, int empCheck)
+        {
+            if(empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Not Present");
+            }
+        }
+    }
+}
